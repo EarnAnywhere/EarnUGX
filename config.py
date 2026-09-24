a648ed15de9fe41692637ff7adb2f2fc8e0bc826
EarnAnywhere <goddessoffire666@gmail.com>
2026-09-25 01:36:38 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,14 @@
+import os
+
+class Config:
+    SECRET_KEY = os.environ.get(
+        "SECRET_KEY",
+        "change-this-secret-key"
+    )
+
+    SQLALCHEMY_DATABASE_URI = os.environ.get(
+        "DATABASE_URL",
+        "sqlite:///lite_ugx_earn.db"
+    )
+
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
